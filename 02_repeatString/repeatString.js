1db35acaf002a362b0d5ab6b105794794c85bb6a
Sam-WebP <samuel-@email.com>
2022-10-19 12:54:42 +1100

    Finishe the second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num > 0 && num != 0) {
+        let combined = [string]; 
+        for (let i = 1; i < num; i++) {
+        combined.push(string)
+        }
+        let noSpace = combined.join("")
+        return noSpace
+    } else if (num == 0) {
+        return "";
+    } else 
+      return "ERROR"
+    
 
 };
 
