43a2a98121a614dad553d7da2b05199dc193380a
Sam-WebP <samuel-@email.com>
2022-10-19 14:01:07 +1100

    Finished the third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let stringArray = [];
+for (let i = string.length; i >= 0; i--) {
+  stringArray.push(string.charAt(i))
+}
+let reversed = stringArray.join("")
+return reversed
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
