173778ed8ff90512e94e087c4f9e7255fc454249
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2022-10-26 10:14:43 +1100

    Finished all the tests for sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,34 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
 
-};
+    let numbersBetween = []
+    let numberSum = 0
+    
+    if (firstNumber <= -1 || secondNumber <= -1) {
+            return "ERROR"
+        } 
+
+    else if (typeof firstNumber != "number" || typeof secondNumber != "number") {
+            return "ERROR"
+        }     
+        
+    else if(firstNumber > secondNumber) {
+        for (let i = firstNumber; i >= secondNumber; i--) {
+            numbersBetween.push(i)
+            numberSum += i 
+    } 
+    return numberSum
+    } 
+    
+    else if(firstNumber <= secondNumber) {
+        for (let i = firstNumber; i <= secondNumber; i++) {
+            numbersBetween.push(i)
+            numberSum += i
+    }
+    return numberSum
+    }
+    };
+    
+    sumAll(1, 4)
 
 // Do not edit below this line
 module.exports = sumAll;
