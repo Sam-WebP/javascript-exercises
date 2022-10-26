b14ac18d0155d3612e5a6c08312e7e0462742a83
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2022-10-26 11:00:18 +1100

    Finished all tests for leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,38 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+if(year % 4 == 0 && (year % 100 == 0 && year % 400 != 0)) {
+        return false
+} else if(year % 4 == 0 && (year % 100 == 0 && year % 400 == 0)) {
+        return true
+} else if(year % 4 == 0 && (year % 100 != 0)) {
+    return true
+} else {
+    return false
+}
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
+//Leap Years are: 
+//Divisible by 4
+//Not devisible 100 unless it is divisable by 400 
+//
+
+/*
+const leapYears = function(year) {
+
+if(year % 4 == 0) {
+    if(year % 100 == 0 && year % 400 != 0)
+        return false
+} else if(year % 4 == 0) {
+    if(year % 100 == 0 && year % 400 == 0)
+        return true
+} else if(year % 4 == 0) {
+    if(year % 100 != 0)
+    return true
+} else {
+    return false
+}
+}; */
