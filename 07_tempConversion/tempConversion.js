4f62560594308ae7f5b6e32c2d2bbe65f83661af
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2022-10-26 11:41:35 +1100

    Passed all the test for tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,22 @@
-const ftoc = function() {
+const ftoc = function(f) {
+let celsius
+  if(f <= -1) {
+  celsius = (f + -32) / 1.8
+  return Math.round(celsius * 10) / 10
+} else 
+  celsius = (f - 32) / 1.8
+  return Math.round(celsius * 10) / 10
 
 };
 
-const ctof = function() {
+const ctof = function(c) {
+
+let fahrenheit = c * 1.8 + 32
+return Math.round(fahrenheit * 10) / 10
 
 };
+
+ctof(0)
 
 // Do not edit below this line
 module.exports = {
