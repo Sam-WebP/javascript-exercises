22a1c49fb34e8128bd4acf029658b2dba9e236e8
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2022-12-26 18:40:56 +1100

    Finished all the exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if(array.length >= 1) {
+    return array.reduce((total, prop) => total + prop);
+  } else {
+    return 0;
+  }
 };
 
-const multiply = function() {
 
+const multiply = function(array) {
+  return array.reduce((total, prop) => total * prop);
+}; 
+
+const power = function(num, power) {
+	return Math.pow(num, power);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = []
+  if(num == 0) {
+    return 1;
+  } else {
+	for (let i = (num); i >= 0.5; i--) {
+    total.unshift(i);
+  }
+  let factor = total.reduce(function(acc, cur) {
+    return acc * cur;
+  });
+  return factor;
+  }
 };
 
 // Do not edit below this line
