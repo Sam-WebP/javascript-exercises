9561967b4549765d8034efd985b8ef353b34089d
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2022-12-26 22:21:55 +1100

    Finished the getTheTitles challenge

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titleArr = []; 
+	books.forEach(function(book) {
+  	let title = book.title;
+    titleArr.push(title);
+  });
+  return titleArr;
 };
-
 // Do not edit below this line
 module.exports = getTheTitles;
