5c9d1554d502c5c56333880f6e87ff770471546e
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2022-12-26 21:58:56 +1100

    Finished the fibonacci challenge

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,31 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+	if(number < 1) {
+        return "OOPS";
+    } else {
+    let fib = [1, 1, 2, 3]
+	for (let i = 3; i <= number; i++) {
+  	let nextNum = fib[i - 1] + fib[i];
+    fib.push(nextNum);
+    }
+  return fib[number - 1];
+  }
 };
+
+
+
+
+
+
+
+
+
+
+
+//create an array that start with (1, 1, 2, 3)
+//make the loop
+//create a loop that adds the array.slice(0,-1) and array.slice(0,-2)
+//input that value into the array.push()
+
 
 // Do not edit below this line
 module.exports = fibonacci;
